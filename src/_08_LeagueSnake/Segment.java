4d8f18d305b67148508409ce864c48665ddd609f
league <league@imac-28.attlocal.net>
2023-12-23 12:49:10 -0800

    bbgffgbfgbdd dfdf

--- src/_08_LeagueSnake/Segment.java
+++ src/_08_LeagueSnake/Segment.java
@@ -1,22 +1,18 @@
 package _08_LeagueSnake;
 
-/*
- * This class will be used to represent each part of the moving snake.
- * 
- * 1. Add x and y member variables.
- *    They will hold the corner location of each segment of the snake.
- * 
- * 2. Add a constructor with parameters to initialize each variable.
- */
+
+// This class will be used to represent each part of the moving snake.
+
+// 1. Add x and y member variables. They will hold the corner location of each segment of the snake.
+
+// 2. Add a constructor with parameters to initialize each variable.
+
 public class Segment {
+    int x;
+    int y;
+    Segment(int x, int y){
+   
 
-	public Segment(int i, int j) {
-		// TODO Auto-generated constructor stub
-	}
+    }
 
-	public void Segment1(int i, int j) {
-		
-		// TODO Auto-generated constructor stub
-	}
-    
 }
